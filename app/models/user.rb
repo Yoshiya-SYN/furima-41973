9cb0b2c148d4b5d3117000028a52d08090008d2d
Yoshiya-SYN <harumakithecat@yahoo.co.jp>
2025-02-28 14:14:53 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }
   validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }
-  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "Input full-width katakana characters" }
-  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "Input full-width katakana characters" }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters' }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters' }
   validates :birth_date, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
 end
