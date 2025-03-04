2014a3d4b5627927c7026255e0129efe8461b0df
Yoshiya-SYN <harumakithecat@yahoo.co.jp>
2025-03-04 15:10:17 +0900

    商品出品モデル作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  validates :title, :description, :price, presence: true
+
+  validates :category_id,      numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id,     numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_fee_id,  numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id,    numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :title, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
 end
