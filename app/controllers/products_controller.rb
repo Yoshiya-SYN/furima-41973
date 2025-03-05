1e6cedb1c38ba53c2560642901aea705a32bef21
Yoshiya-SYN <harumakithecat@yahoo.co.jp>
2025-03-05 13:17:23 +0900

    商品出品バリデーション作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,36 @@
 class ProductsController < ApplicationController
+
+  before_action :move_to_login, only: [:new]
+
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :title, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,18 +9,14 @@ class Product < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
-  validates :title, :description, :price, presence: true
+  validates :title, :description, :price, :image, presence: true
 
   validates :category_id,      numericality: { other_than: 1 , message: "can't be blank" }
   validates :condition_id,     numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_fee_id,  numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id,    numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :price,            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, }
 
-  private
-
-  def product_params
-    params.require(:product).permit(:image, :title, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
 
 end
